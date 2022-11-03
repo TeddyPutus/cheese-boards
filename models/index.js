e160558603d6acfb4bd10d487d6df211ea413ddb
Teddy Putus <99917582+TeddyPutus@users.noreply.github.com>
2022-11-03 15:20:15 +0000

    created associations and blank file for testing associations

--- models/index.js
+++ models/index.js
@@ -2,6 +2,12 @@ const User = require('./user.model');
 const Board = require('./board.model');
 const Cheese = require('./cheese.model');
 
-//any associations and set up will go here
+//One to Many association - Multiple boards can be added to a User
+Board.belongsTo(User);
+User.hasMany(Board);
+
+//Many to Many association - A board can have many cheeses, and a cheese can be on many boards
+Board.belongsToMany(Cheese, {through: "Board_Cheese"});
+Cheese.belongsToMany(Board, {through: "Board_Cheese"});
 
 module.exports = { User, Board, Cheese};
